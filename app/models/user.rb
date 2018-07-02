e87262c8ca39306b06ee5563b8e322ee28242da6
Isaac Orme <isorme1@gmail.com>
2018-07-02 14:55:48 -0700

    Adds role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+
+  before_save  { self.role ||= :member }
+
+  enum role: [:member, :admin]
 end
