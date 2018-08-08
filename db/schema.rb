3890251b4a44b54ad10c74d4c96cb507bb95a549
Isaac Orme <isorme1@gmail.com>
2018-08-08 10:24:18 -0700

    Adds ability to use both username and email for login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_12_190941) do
+ActiveRecord::Schema.define(version: 2018_08_08_170026) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string "data_file_name", null: false
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2018_07_12_190941) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
